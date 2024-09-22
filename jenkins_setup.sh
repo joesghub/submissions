fa0f540e82041c57c06cfef5600249ef7c4206bb
Joe Reynolds <43896689+joesghub@users.noreply.github.com>
2024-09-21 17:39:10 -0700

    Update jenkins_setup.sh

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -3,10 +3,12 @@
 # Update system and install necessary packages
 sudo apt update && sudo apt install -y fontconfig openjdk-17-jre software-properties-common
 
-# Add Python PPA for Python 3.7 and install Python
+# Add Python PPA for Python 3.9 and install Python
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install -y python3.7 python3.7-venv
+
+# Install Python 3.9, Python 3.9 venv, pip, and nginx
+sudo apt install -y python3.9 python3.9-venv python3-pip nginx
 
 # Download Jenkins key and add Jenkins repository
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
