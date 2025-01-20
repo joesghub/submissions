589f54ed8337ffc2cbf5bb1aa432ae20533761d9
Joe Reynolds <43896689+joesghub@users.noreply.github.com>
2025-01-19 21:32:38 -0800

    Update install_aws_cli.sh

--- install_aws_cli.sh
+++ install_aws_cli.sh
@@ -7,8 +7,7 @@ sudo apt-get install -y unzip
 # Step 2: Download the AWS CLI v2 installer
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
-# Step 3: Download unzip and open the downloaded file
-sudo apt install unzip
+# Step 3: Open the downloaded file
 unzip awscliv2.zip
 
 # Step 4: Run the installation script
